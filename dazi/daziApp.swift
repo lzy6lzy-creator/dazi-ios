6d759f19f8e6286ccd77b6aa1d7e831922fc98ca
wuxing <wuxing@xiaohongshu.com>
2026-03-06 01:43:54 +0800

    Initial Commit

---
+++ dazi/daziApp.swift
@@ -0,0 +1,17 @@
+//
+//  daziApp.swift
+//  dazi
+//
+//  Created by 林客(吴星) on 2026/3/6.
+//
+
+import SwiftUI
+
+@main
+struct daziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
